c1d84bec771f5137f16f3329e013755dab3f78f2
sandeep joshi <joshi.sandeep.d@gmail.com>
2016-01-25 14:40:27 +0530

    fix typo

--- imageApi.js
+++ imageApi.js
@@ -9,8 +9,6 @@ router.post('/upload',  function (req, res) {
 
   var form = new multiparty.Form();
   form.parse(req, (err, fields, files) => {
-	  // console.log(fields.files[0]);
-	  // console.log(files);
 
     var data_url = fields.horizontal[0];
     data_url = data_url.replace(/^data:image\/jpeg+;base64,/, "");
--- src/components/fileUploader.jsx
+++ src/components/fileUploader.jsx
@@ -11,7 +11,7 @@ export default class FileUploader extends React.Component {
     this.state = {
       errMsg: '',
       src : '',
-      sucessMsg : ''
+      successMsg : ''
     }
   };
 
@@ -39,7 +39,6 @@ export default class FileUploader extends React.Component {
 
   handleSubmit(e){
     e.preventDefault();
-
     this.uploadImage(this.state.src);
   }
 
@@ -55,7 +54,7 @@ export default class FileUploader extends React.Component {
 
     var regexImage = /^(image\/jpeg|image\/png|image\/gif|image\/bmp|image\/jpg|image\/pjpeg)$/i;
     if (!regexImage.test(files[0].type)) {
-      this.setState({ errMsg : 'Please select a valid image file' , sucessMsg : '' });
+      this.setState({ errMsg : 'Please select a valid image file' , successMsg : '' });
       return;
     }
 
@@ -65,11 +64,11 @@ export default class FileUploader extends React.Component {
       image.src = reader.result;
       image.onload = () => {
         if( image.height !== 1024 && image.width !== 1024 ) {
-          this.setState({ errMsg : "Please upload the file of size 1024 * 1024" , sucessMsg : '' });
+          this.setState({ errMsg : "Please upload the file of size 1024 * 1024" , successMsg : '' });
           return;
         }
         // trigger action.
-        this.setState({src: reader.result , errMsg : '' , sucessMsg : 'Change the Crop area using bellow preview.'});
+        this.setState({src: reader.result , errMsg : '' , successMsg : 'Change the Crop area using bellow preview.'});
       };
     };
     reader.readAsDataURL(files[0]);
@@ -77,11 +76,11 @@ export default class FileUploader extends React.Component {
 
   render() {
     return(
-      <form className="file-uploader" enctype="multipart/form-data" onSubmit={this.handleSubmit.bind(this)}>
+      <form className="file-uploader" onSubmit={this.handleSubmit.bind(this)}>
         <div className="line">Select image, crop it and upload.</div>
         <input type='file' onChange={this.onFileSelect} />
         <div className="error-message line">{this.state.errMsg}</div>
-        <div className="line">{this.state.sucessMsg}</div>
+        <div className="line">{this.state.successMsg}</div>
         <input type="submit" value="Crop & Upload" />
       </form>
     )
