9553da6382d1abd4da098026571b85372190461d
sandeep joshi <joshi.sandeep.d@gmail.com>
2016-01-26 14:13:16 +0530

    change default value

--- src/reducers/image.js
+++ src/reducers/image.js
@@ -11,7 +11,7 @@ const initialState = {
   pathVertical : '' ,
   pathHorizontalSmall : '' ,
   pathGallary : '' ,
-  loading : true,
+  loading : false,
 };
 
 export default createReducer(initialState, {
